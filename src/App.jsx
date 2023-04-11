011d1c409f94c9c4114230341f1e373e8c11a390
isactes <fidel.renovato@gmail.com>
2023-04-10 22:34:03 -0600

    make a componente

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TwitterFollowCard  from "./TwitterFollowCard"
+
+function App() {
+    return(
+        <div className="App">
+        <TwitterFollowCard userName="isactes" name="isac" />
+        <TwitterFollowCard userName="adasd" name="isac" />
+        <TwitterFollowCard userName="isactesdasdas" name="isac" />
+        <TwitterFollowCard userName="isactesdadsa" name="isac" />
+        </div>
+    )
+}
+
+export  default App;
---
+++ src/TwitterFollowCard.jsx
@@ -0,0 +1,26 @@
+import "./App.css"
+
+function TwitterFollowCard({ userName, name, isFollowimg }) {
+
+    //const srcImg = `https://unavatar.io/telegram/${userName}`
+    return (
+        <article className="tw-followCard">
+            <header className="tw-followCard-hedaer">
+                <img
+                className="tw-followCard-img" 
+                src={`https://unavatar.io/telegram/${userName}`}  alt="Avatar"/>
+                <div className="tw-followCard-infoUser">
+                    <strong>Isac Renovato</strong>
+                    <span className="tw-followCard-useName">@{userName}</span>
+                </div>
+            </header>
+            <aside className="tw-followCard-aside">
+                <button className="tw-followCard-button">
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
+
+export default TwitterFollowCard;
