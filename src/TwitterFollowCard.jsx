aac611ff04cf073eea81a34b9bd1eed50b708458
isactes <fidel.renovato@gmail.com>
2023-04-14 22:58:43 -0600

    we can doing this form but it better useState

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+
 import "./App.css"
 
-function TwitterFollowCard({ children, userName, name, isFollowimg }) {
+
+function TwitterFollowCard({ children, userName, isFollowimg }) {
 
     //const srcImg = `https://unavatar.io/telegram/${userName}`
     //const addAt = (userName) => `@${userName}`
     //Componentes crean elementos y los elemntos son los que renderiza React
     //Componente funcion que devuelve un elemento y React redenriza el elemento
+    const text = isFollowimg ? "Siguiendo" : "Seguir"
+    const buttonClassName = isFollowimg ? "tw-followCard-button is-folowing" : "tw-followCard-button"
+
+    
+
+
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-hedaer">
@@ -15,13 +24,13 @@ function TwitterFollowCard({ children, userName, name, isFollowimg }) {
                 <div className="tw-followCard-infoUser">
                     <strong>{children}</strong>
                     <span className="tw-followCard-useName">
-                        {userName}
+                        @{userName}
                     </span>
                 </div>
             </header>
             <aside className="tw-followCard-aside">
-                <button className="tw-followCard-button">
-                    Seguir
+                <button className={buttonClassName}>
+                    {text}
                 </button>
             </aside>
         </article>
