7e88b4d49ef44c6f4f71ff09e8cfd0bdf0493201
isactes <fidel.renovato@gmail.com>
2023-04-17 22:56:16 -0600

    one way to render

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,56 @@ function App() {
 //const [isFolloeing, setIsFollowing] = useState(false)
   //error nunca inicializa un stado para que se puede cambiar desde el padre, si el hijo esta con un estado inical se quea fijo ya que no podemos cambiar el estado al padre si el hijo esta inicializado el estado
 
+  const users = [
+    {
+        userName: "isactes",
+        name:"isac",
+        isFollowing: false
+    },
+    {
+        userName: "fideless",
+        name:"fidel",
+        isFollowing: true
+    },
+    {
+        userName: "esmemon",
+        name:"esme",
+        isFollowing: false
+    },
+    {
+        userName: "chiossa",
+        name:"chio",
+        isFollowing: true
+    },
+    {
+        userName: "wallas",
+        name:"wallas",
+        isFollowing: false
+    },
+    {
+        userName: "auroramusic",
+        name:"aurora",
+        isFollowing: true
+    },
+  ]
+
     return(
         <div className="App">
         {/*poner comentarios */}
-
-        <TwitterFollowCard  userName="isactes">
-            Isac 
-        </TwitterFollowCard>
-
-        <TwitterFollowCard initialIsItFollowing  userName="fidel">
-            Fidel 
-        </TwitterFollowCard>
+        {/*una manera de maperar y renderizar */}
+        {
+            users.map(user => {
+                const { userName, name, isFollowing } = user
+                return(
+                    <TwitterFollowCard
+                    userName={userName}
+                    initialIsItFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                )
+            })
+        }
 
         </div>
     )
